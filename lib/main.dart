7351b3bbfc8a4f95fe7966cd0cffa6ae29ba6e80
ZERONE <zerone012@gmail.com>
2023-05-18 10:26:34 +0900

    Firebase 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:study_flutter_08_codingchef05_chat/screen/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
