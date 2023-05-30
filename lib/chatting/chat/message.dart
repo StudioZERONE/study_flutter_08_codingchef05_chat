c85dc1a9181ab8aad20e991d1685630b169a875b
ZERONE <zerone012@gmail.com>
2023-05-30 09:58:57 +0900

    메시지 상하 정렬을 반대로 reverse

--- lib/chatting/chat/message.dart
+++ lib/chatting/chat/message.dart
@@ -10,15 +10,16 @@ class Messages extends StatelessWidget {
       stream: FirebaseFirestore.instance.collection('chat').snapshots(),
       builder: (context,
           AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+        // if (snapshot.connectionState == ConnectionState.waiting) {
+        //   return const Center(
+        //     child: CircularProgressIndicator(),
+        //   );
+        // }
 
         final chatDocs = snapshot.data!.docs;
 
         return ListView.builder(
+          reverse: true,
           itemCount: chatDocs.length,
           itemBuilder: (context, index) {
             return Text(chatDocs[index]['text']);
