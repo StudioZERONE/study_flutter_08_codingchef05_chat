9430e290f3d0429186f9d823c6679f2c41cf7108
ZERONE <zerone012@gmail.com>
2023-06-06 10:11:15 +0900

    챗버블 UI 조금 바꿈

--- lib/chatting/chat/chat_bubble.dart
+++ lib/chatting/chat/chat_bubble.dart
@@ -41,33 +41,39 @@ class ChatBubbles extends StatelessWidget {
         //   ),
         // ),
         if (isMe)
-          ChatBubble(
-            clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
-            alignment: Alignment.topRight,
-            margin: const EdgeInsets.only(top: 20),
-            backGroundColor: Colors.blue,
-            child: Container(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.7,
-              ),
-              child: Text(
-                message,
-                style: const TextStyle(color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+            child: ChatBubble(
+              clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
+              alignment: Alignment.topRight,
+              margin: const EdgeInsets.only(top: 20),
+              backGroundColor: Colors.blue,
+              child: Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.7,
+                ),
+                child: Text(
+                  message,
+                  style: const TextStyle(color: Colors.white),
+                ),
               ),
             ),
           )
         else
-          ChatBubble(
-            clipper: ChatBubbleClipper8(type: BubbleType.receiverBubble),
-            backGroundColor: const Color(0xffE7E7ED),
-            margin: const EdgeInsets.only(top: 20),
-            child: Container(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.7,
-              ),
-              child: Text(
-                message,
-                style: const TextStyle(color: Colors.black),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+            child: ChatBubble(
+              clipper: ChatBubbleClipper8(type: BubbleType.receiverBubble),
+              backGroundColor: const Color(0xffE7E7ED),
+              margin: const EdgeInsets.only(top: 20),
+              child: Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.7,
+                ),
+                child: Text(
+                  message,
+                  style: const TextStyle(color: Colors.black),
+                ),
               ),
             ),
           )
